c6075ca3f550a3caf20398e9520c3ed2315c7efa
connorbillen <me@connorbillen.com>
2021-02-13 21:41:36 -0500

    Working add/progress boost

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -5,17 +5,16 @@ import * as React from 'react'
 const ProgressBar = (props: { progress: number, add?: number }): JSX.Element => {
 
     return (
-        <View style={ !props.add ? 
-            styles.progressContainer :
+        <View style={ styles.progressContainer }>
+            <View style={{ ...styles.progress, width: styles.progressContainer.width * (props.progress / 100) }}></View>
             {
-                ...styles.progressContainer,
-                width: props.progress + props.add,
-                backgroundColor: 'rgba(255, 199, 184, 0.5)',
-            }}>
-            <View style={ !props.add ? 
-                { ...styles.progress, width: styles.progressContainer.width * (props.progress / 100) } :
-                { ...styles.progress, width: props.progress }
-            }></View>
+                props.add &&
+                <View style={{
+                    ...styles.progress,
+                    width: styles.progressContainer.width * ((props.progress + props.add) / 100),
+                    backgroundColor: 'rgba(255, 199, 185, 0.5)',
+                }}></View>
+            }
         </View>
     )
 }
@@ -26,12 +25,16 @@ const styles = StyleSheet.create({
         width: 250,
         backgroundColor: 'rgba(255, 255, 255, 1.0)',
         borderRadius: 5,
+        borderColor: 'rgba(0, 0, 0, 0.3)',
+        borderStyle: 'solid',
+        borderWidth: 1,
     },
     progress: {
         width: 0,
         height: '100%',
         borderRadius: 5,
-        backgroundColor: '#ffc7b8',
+        backgroundColor: 'rgba(255, 199, 185, 1.0)',
+        position: 'absolute',
     },
 })
 
